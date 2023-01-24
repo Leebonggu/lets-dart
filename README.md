3d53d1cce7fdffb52ddba23ffe933fdb7bcdd47b
Leebonggu <leebonggu12@hanmail.net>
2023-01-24 22:12:39 +0900

    end

--- README.md
+++ README.md
@@ -16,3 +16,34 @@
 - 지역변수를 선언할 때는 var
 - class -> Type
 - 언제든 업데이트 가능
+
+### Nullable
+- null safety -> 런타임 에러를 방어해줌
+- 기본적으로 모든 값은 Non-nullable
+- ? 를 통해 옵셔널 값으로 변경가능
+
+## 데이터 타입
+
+- String
+- bool
+- int
+- double: 실수
+- num: int or double
+  - int, double in num
+
+- 대부분의 타입이 object
+- list -> like array
+
+- Map
+  - Like Object
+  - Like Dict (in python)
+  - key/value -> class가 더 좋다?
+- Set
+  - var numbers = {1, 2, 3, 4, 5};
+
+## Function
+
+## Class
+
+### Enum
+- 실수를 줄여줌
--- main.dart
+++ main.dart
@@ -1,7 +1,104 @@
+bool isEmpty(String string) => string.length == 0;
+
 void main() {
-  String name = 'Bonggu';
+  // String name = 'Bonggu';
+  // print(name);
+
+  // var name2 = 'Bonggu2';
+  // print(name2);
+
+  // isEmpty(null) // The argument type 'Null' can't be assigned to the parameter type 'String'
+
+  // String? bong = 'bong';
+  // bong = null;
+
+  // if (bong != null) {
+  //   bong.length;
+  // }
+  // bong?.length;
+  var giveMeFive = true;
+  var number = [
+    1,
+    2,
+    3,
+    4,
+    if (giveMeFive) 5,
+  ];
+
+  var numbersSet = {
+    1,
+    2,
+    3,
+    4,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+  };
+  print(numbersSet);
+  //  function
+  sayHello('Hello');
+  sayReturnHello('Hello');
+  print(sayHello2(age: 12, country: 'HelloCountry', name: 'HelloName'));
+  String? name;
+  name ??= 'Bong2';
   print(name);
 
-  var name2 = 'Bonggu2';
-  print(name2);
+  var player = Player(
+    name: 'helelo',
+    xp: 1,
+    team: Team.red,
+    age: 10,
+  );
+  print(player.name);
+  print(player.xp);
+  print(player.team);
+  print(player.age);
+  player.sayHello();
+}
+
+enum Team { red, blue }
+
+class Player {
+  final String name;
+  // final String name = 'Bong';
+  int xp, age;
+  Team team;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
+
+  void sayHello() {
+    print('My name is ${this.name}');
+  }
+}
+
+void sayHello(String name) {
+  print('Hello, World function $name');
+}
+
+String sayReturnHello(String name) {
+  return 'Hello, $name';
+}
+
+String sayHello2({required String name, int? age, String? country}) {
+  return 'Helo $name $age $country';
+}
+
+String capitalizedName(String? name) => name?.toLowerCase() ?? 'ANon';
+
+typedef ListOfInt = List<int>;
+ListOfInt reverseNumbers(ListOfInt ll) {
+  var res = ll.reversed;
+  return res.toList();
 }
